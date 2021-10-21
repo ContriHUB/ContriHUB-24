9a16d9a733c3f95cdaec6ce791b6c92dd30db042
CyrilMishra <mishracyril@gmail.com>
2021-10-21 23:02:20 +0530

    Issue #41 solved

--- project/models.py
+++ project/models.py
@@ -33,6 +33,19 @@ class Project(models.Model):
 
     domain = models.ForeignKey(Domain, on_delete=models.DO_NOTHING, null=True, default=None)
 
+    def sub_domains(project_id):
+        sub_domain_qs = SubDomainProject.objects.filter(project=project_id)
+        sd = sub_domain_qs.all()
+        sub_domains = ''
+        act_sub_domains = ''
+        if (len(sd) > 0):
+            for s in sd:
+                sub_domains += s.sub_domain.name.__str__() + '/'
+            act_sub_domains = sub_domains[:-1]
+            return '(' + act_sub_domains + ')'    # ( subdomains )
+        else:
+            return ''
+
     def __str__(self):
         return self.name
 
