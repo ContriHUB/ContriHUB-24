550960d2f6286ef58307d9d517e96c820430ed67
DuP-491 <divyansh.20198117@mnnit.ac.in>
2022-10-01 17:14:31 +0530

    exception fix

--- project/views.py
+++ project/views.py
@@ -62,8 +62,13 @@ def populate_issues(request):
             for issue in issues:
                 # print(issue)
                 # TODO: Can be given as ISSUE
-                if issue['user']['login'] == DEPENDABOT_LOGIN:  # Ignoring issues created by Dependabot
+                try:
+                    if issue['user']['login'] == DEPENDABOT_LOGIN:  # Ignoring issues created by Dependabot
+                        continue
+                except:
                     continue
+                finally:
+                    print('error')
                 if issue.get('pull_request') is not None:  # this issue is actually a PR.
                     # Source: https://docs.github.com/en/rest/reference/issues#list-repository-issues
                     print("This issue is a actually a PR")
@@ -128,7 +133,8 @@ def parse_level(level):
     level = str(level).lower()
     levels_read = (Issue.FREE_READ, Issue.VERY_EASY_READ, Issue.EASY_READ, Issue.MEDIUM_READ, Issue.HARD_READ)
     levels = (Issue.FREE, Issue.VERY_EASY, Issue.EASY, Issue.MEDIUM, Issue.HARD)
-    default_points = (DEFAULT_FREE_POINTS, DEFAULT_VERY_EASY_POINTS, DEFAULT_EASY_POINTS, DEFAULT_MEDIUM_POINTS, DEFAULT_HARD_POINTS)
+    default_points = (
+    DEFAULT_FREE_POINTS, DEFAULT_VERY_EASY_POINTS, DEFAULT_EASY_POINTS, DEFAULT_MEDIUM_POINTS, DEFAULT_HARD_POINTS)
 
     for lev, read, pts in zip(levels, levels_read, default_points):
         if level == str(read).lower():
