ce941853a41d3cfe843bda7ad2d7c8ffa55d4fbf
Krishna Soni <92522740+Krishna1922@users.noreply.github.com>
2022-10-13 10:30:32 +0530

    Update admin.py

--- project/admin.py
+++ project/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import *
+from .models import Dislike, Like, Project, Issue, PullRequest, IssueAssignmentRequest, ActiveIssue, Domain, SubDomain
+
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'domain', 'html_url')
@@ -21,20 +22,12 @@ class ActiveIssueAdmin(admin.ModelAdmin):
     list_display = ('contributor', 'issue', 'assigned_at')
 
 
-class LikesAdmin(admin.ModelAdmin):
-    list_display = ('issue', 'user')
-
-class DislikesAdmin(admin.ModelAdmin):
-    list_display = ('issue', 'user')
-
-    
-admin.site.register(Likes, LikesAdmin)
-admin.site.register(Dislikes, DislikesAdmin)
 admin.site.register(Project, ProjectAdmin)
-
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(PullRequest, PullRequestAdmin)
 admin.site.register(IssueAssignmentRequest, IssueAssignmentRequestAdmin)
 admin.site.register(ActiveIssue, ActiveIssueAdmin)
 admin.site.register(Domain)
 admin.site.register(SubDomain)
+admin.site.register(Like)
+admin.site.register(Dislike)
