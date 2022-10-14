2bf3981a9d53d9a246610c53b475acc8acf3e437
Krishna Soni <92522740+Krishna1922@users.noreply.github.com>
2022-10-14 10:17:03 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -3,9 +3,8 @@ from django.core.mail import send_mail, BadHeaderError
 
 from home.helpers import EmailThread
 from django.core import mail
-from project.models import Project, Issue, IssueAssignmentRequest, Like
+from project.models import Project, Issue, IssueAssignmentRequest, Like, ActiveIssue, PullRequest, Domain, SubDomain, Dislike
 
-from project.models import ActiveIssue, PullRequest, Domain, SubDomain, Dislike
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from helper import complete_profile_required, check_issue_time_limit
